daeb1a01f0f429796e215329143f9eac669ca775
Fatur Akbar <faturakbar03@gmail.com>
2020-10-20 16:12:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,5 @@
 <?php 
-	$koneksi = mysqli_connect("sql206.epizy.com","epiz_27002752","1OFmswRPn54
-	","epiz_27002752_db_kasir");
-
+	$koneksi =mysqli_connect("localhost","root","","db_kasir");
 	if (mysqli_connect_error()) {
 		echo "koneksi database Gagal". mysqli_connect_error();
 	}
